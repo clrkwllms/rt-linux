3fc8d0336d3573ff66f032076b7bcf01ea5fa31c
Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
2008-05-12 21:21:09 +0200

    Immediate Values - x86 Optimization NMI and MCE support
    x86 optimization of the immediate values which uses a movl with code patching
    to set/unset the value used to populate the register used as variable source.
    It uses a breakpoint to bypass the instruction being changed, which lessens the
    interrupt latency of the operation and protects against NMIs and MCE.
    
    - More reentrant immediate value : uses a breakpoint. Needs to know the
      instruction's first byte. This is why we keep the "instruction size"
      variable, so we can support the REX prefixed instructions too.
    
    Changelog:
    - Change the immediate.c update code to support variable length opcodes.
    - Use text_poke_early with cr0 WP save/restore to patch the bypass. We are doing
      non atomic writes to a code region only touched by us (nobody can execute it
      since we are protected by the imv_mutex).
    - Add x86_64 support, ready for i386+x86_64 -> x86 merge.
    - Use asm-x86/asm.h.
    - Change the immediate.c update code to support variable length opcodes.
    - Use imv_* instead of immediate_*.
    - Use kernel_wp_disable/enable instead of save/restore.
    - Fix 1 byte immediate value so it declares its instruction size.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
    CC: Andi Kleen <ak@muc.de>
    CC: "H. Peter Anvin" <hpa@zytor.com>
    CC: Chuck Ebbert <cebbert@redhat.com>
    CC: Christoph Hellwig <hch@infradead.org>
    CC: Jeremy Fitzhardinge <jeremy@goop.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/immediate.h
+++ include/asm-x86/immediate.h
@@ -12,6 +12,18 @@
 
 #include <asm/asm.h>
 
+struct __imv {
+	unsigned long var;	/* Pointer to the identifier variable of the
+				 * immediate value
+				 */
+	unsigned long imv;	/*
+				 * Pointer to the memory location of the
+				 * immediate value within the instruction.
+				 */
+	unsigned char size;	/* Type size. */
+	unsigned char insn_size;/* Instruction size. */
+} __attribute__ ((packed));
+
 /**
  * imv_read - read immediate variable
  * @name: immediate value name
@@ -26,6 +38,11 @@
  * what will generate an instruction with 8 bytes immediate value (not the REX.W
  * prefixed one that loads a sign extended 32 bits immediate value in a r64
  * register).
+ *
+ * Create the instruction in a discarded section to calculate its size. This is
+ * how we can align the beginning of the instruction on an address that will
+ * permit atomic modification of the immediate value without knowing the size of
+ * the opcode used by the compiler. The operand size is known in advance.
  */
 #define imv_read(name)							\
 	({								\
@@ -33,9 +50,14 @@
 		BUILD_BUG_ON(sizeof(value) > 8);			\
 		switch (sizeof(value)) {				\
 		case 1:							\
-			asm(".section __imv,\"aw\",@progbits\n\t"	\
+			asm(".section __discard,\"\",@progbits\n\t"	\
+				"1:\n\t"				\
+				"mov $0,%0\n\t"				\
+				"2:\n\t"				\
+				".previous\n\t"				\
+				".section __imv,\"aw\",@progbits\n\t"	\
 				_ASM_PTR "%c1, (3f)-%c2\n\t"		\
-				".byte %c2\n\t"				\
+				".byte %c2, (2b-1b)\n\t"		\
 				".previous\n\t"				\
 				"mov $0,%0\n\t"				\
 				"3:\n\t"				\
@@ -45,10 +67,16 @@
 			break;						\
 		case 2:							\
 		case 4:							\
-			asm(".section __imv,\"aw\",@progbits\n\t"	\
-				_ASM_PTR "%c1, (3f)-%c2\n\t"		\
-				".byte %c2\n\t"				\
+			asm(".section __discard,\"\",@progbits\n\t"	\
+				"1:\n\t"				\
+				"mov $0,%0\n\t"				\
+				"2:\n\t"				\
 				".previous\n\t"				\
+				".section __imv,\"aw\",@progbits\n\t"	\
+				_ASM_PTR "%c1, (3f)-%c2\n\t"		\
+				".byte %c2, (2b-1b)\n\t"		\
+				".previous\n\t"				\
+				".org . + ((-.-(2b-1b)) & (%c2-1)), 0x90\n\t" \
 				"mov $0,%0\n\t"				\
 				"3:\n\t"				\
 				: "=r" (value)				\
@@ -60,10 +88,16 @@
 				value = name##__imv;			\
 				break;					\
 			}						\
-			asm(".section __imv,\"aw\",@progbits\n\t"	\
-				_ASM_PTR "%c1, (3f)-%c2\n\t"		\
-				".byte %c2\n\t"				\
+			asm(".section __discard,\"\",@progbits\n\t"	\
+				"1:\n\t"				\
+				"mov $0xFEFEFEFE01010101,%0\n\t"	\
+				"2:\n\t"				\
 				".previous\n\t"				\
+				".section __imv,\"aw\",@progbits\n\t"	\
+				_ASM_PTR "%c1, (3f)-%c2\n\t"		\
+				".byte %c2, (2b-1b)\n\t"		\
+				".previous\n\t"				\
+				".org . + ((-.-(2b-1b)) & (%c2-1)), 0x90\n\t" \
 				"mov $0xFEFEFEFE01010101,%0\n\t" 	\
 				"3:\n\t"				\
 				: "=r" (value)				\
@@ -73,5 +107,7 @@
 		};							\
 		value;							\
 	})
+
+extern int arch_imv_update(const struct __imv *imv, int early);
 
 #endif /* _ASM_X86_IMMEDIATE_H */
