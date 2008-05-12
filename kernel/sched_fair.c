d40d3be2ea73bea779e2e5ad33d119d03dda525f
Ingo Molnar <mingo@elte.hu>
2008-05-12 15:43:53 +0200

    revert: v2.6.25 2524 g0bbd333
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -63,13 +63,13 @@ unsigned int __read_mostly sysctl_sched_compat_yield;
 
 /*
  * SCHED_OTHER wake-up granularity.
- * (default: 10 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 5 msec * (1 + ilog(ncpus)), units: nanoseconds)
  *
  * This option delays the preemption effects of decoupled workloads
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 10000000UL;
+unsigned int sysctl_sched_wakeup_granularity = 5000000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
@@ -637,7 +637,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 			 * convert the sleeper threshold into virtual time
 			 */
 			if (sched_feat(NORMALIZED_SLEEPER))
-				thresh = calc_delta_fair(thresh, se);
+				thresh = calc_delta_fair(thresh, &se->load);
 
 			vruntime -= thresh;
 		}
@@ -756,16 +756,20 @@ set_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	se->prev_sum_exec_runtime = se->sum_exec_runtime;
 }
 
-static int
-wakeup_preempt_entity(struct sched_entity *curr, struct sched_entity *se);
-
 static struct sched_entity *
 pick_next(struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
+	s64 diff, gran;
+
 	if (!cfs_rq->next)
 		return se;
 
-	if (wakeup_preempt_entity(cfs_rq->next, se) != 0)
+	diff = cfs_rq->next->vruntime - se->vruntime;
+	if (diff < 0)
+		return se;
+
+	gran = calc_delta_fair(sysctl_sched_wakeup_granularity, &cfs_rq->load);
+	if (diff > gran)
 		return se;
 
 	return cfs_rq->next;
@@ -1196,6 +1200,7 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p)
 	struct cfs_rq *cfs_rq = task_cfs_rq(curr);
 	struct sched_entity *se = &curr->se, *pse = &p->se;
 	int se_depth, pse_depth;
+	unsigned long gran;
 
 	if (unlikely(rt_prio(p->prio))) {
 		update_rq_clock(rq);
@@ -1246,7 +1251,15 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p)
 		pse = parent_entity(pse);
 	}
 
-	if (wakeup_preempt_entity(se, pse) == 1)
+	gran = sysctl_sched_wakeup_granularity;
+	/*
+	 * More easily preempt - nice tasks, while not making
+	 * it harder for + nice tasks.
+	 */
+	if (unlikely(se->load.weight > NICE_0_LOAD))
+		gran = calc_delta_fair(gran, &se->load);
+
+	if (pse->vruntime + gran < se->vruntime)
 		resched_task(curr);
 }
 
