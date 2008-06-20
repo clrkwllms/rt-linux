1cf88a861df5643a99da15b184e43615b1da4557
Ingo Molnar <mingo@elte.hu>
2008-06-20 15:38:00 +0200

    ftrace: sched_setscheduler() fix
    fix sched_setscheduler fallout:
    
    In file included from kernel/trace/trace.c:2832:
    kernel/trace/trace_selftest.c: In function 'trace_wakeup_test_thread':
    kernel/trace/trace_selftest.c:425: error: too few arguments to function 'sched_setscheduler'
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace_selftest.c
+++ kernel/trace/trace_selftest.c
@@ -421,7 +421,7 @@ static int trace_wakeup_test_thread(void *data)
 	struct sched_param param = { .sched_priority = 5 };
 	struct completion *x = data;
 
-	sched_setscheduler(current, SCHED_FIFO, &param);
+	sched_setscheduler(current, SCHED_FIFO, &param, false);
 
 	/* Make it know we have a new prio */
 	complete(x);
