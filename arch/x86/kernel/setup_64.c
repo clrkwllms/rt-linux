7f1d5f45386a2b2fb037e43395f5473d8c523c93
Yinghai Lu <yhlu.kernel@gmail.com>
2008-06-21 01:38:41 -0700

    x86: remove some acpi ifdefs in setup_32/64
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/setup_64.c
+++ arch/x86/kernel/setup_64.c
@@ -367,13 +367,11 @@ void __init setup_arch(char **cmdline_p)
 	kvmclock_init();
 #endif
 
-#ifdef CONFIG_ACPI
 	/*
 	 * Initialize the ACPI boot-time table parser (gets the RSDP and SDT).
 	 * Call this early for SRAT node setup.
 	 */
 	acpi_boot_table_init();
-#endif
 
 	/* How many end-of-memory variables you have, grandma! */
 	max_low_pfn = end_pfn;
@@ -444,12 +442,10 @@ void __init setup_arch(char **cmdline_p)
 
 	early_quirks();
 
-#ifdef CONFIG_ACPI
 	/*
 	 * Read APIC and some other early information from ACPI tables.
 	 */
 	acpi_boot_init();
-#endif
 
 	init_cpu_to_node();
 
