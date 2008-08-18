d92ca3e085e9ec39adb20d93bc8266b6fdc83d3d
Suresh Siddha <suresh.b.siddha@intel.com>
2008-08-18 11:13:01 -0700

    x2apic: use cpuid vector 0xb when available for detecting cpu topology
    cpuid leaf 0xb provides extended topology enumeration. This interface provides
    the 32-bit x2APIC id of the logical processor and it also provides a new
    mechanism to detect SMT and core siblings (which provides increased
    addressability).
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/cpufeature.h
+++ include/asm-x86/cpufeature.h
@@ -80,6 +80,7 @@
 #define X86_FEATURE_MFENCE_RDTSC (3*32+17) /* Mfence synchronizes RDTSC */
 #define X86_FEATURE_LFENCE_RDTSC (3*32+18) /* Lfence synchronizes RDTSC */
 #define X86_FEATURE_11AP	(3*32+19)  /* Bad local APIC aka 11AP */
+#define X86_FEATURE_XTOPOLOGY    (3*32+20) /* cpu topology enum extensions */
 
 /* Intel-defined CPU features, CPUID level 0x00000001 (ecx), word 4 */
 #define X86_FEATURE_XMM3	(4*32+ 0) /* Streaming SIMD Extensions-3 */
