857e12c38306faae75d3d65f31f57723a8718803
Ingo Molnar <mingo@elte.hu>
2008-10-05 10:42:55 +0200

    x86: __show_registers() and __show_regs() API unification, fixlet
    fix merge leftover - missing hunk that did not get carried over properly.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/process_64.c
+++ arch/x86/kernel/process_64.c
@@ -175,6 +175,9 @@ void __show_regs(struct pt_regs *regs, int all)
 	rdmsrl(MSR_GS_BASE, gs);
 	rdmsrl(MSR_KERNEL_GS_BASE, shadowgs);
 
+	if (!all)
+		return;
+
 	printk(KERN_INFO "FS:  %016lx(%04x) GS:%016lx(%04x) knlGS:%016lx\n",
 	       fs, fsindex, gs, gsindex, shadowgs);
 
