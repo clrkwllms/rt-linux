39bc66625d7a761f6e5c5b3d81d88e7c9359ca84
Ingo Molnar <mingo@elte.hu>
2009-02-28 10:18:43 +0100

    kmemcheck: fix split_page() of the page allocator, fix
    Impact: build fix for !CONFIG_KMEMCHECK
    
    Cc: Vegard Nossum <vegard.nossum@gmail.com>
    Cc: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1059,12 +1059,14 @@ void split_page(struct page *page, unsigned int order)
 	VM_BUG_ON(PageCompound(page));
 	VM_BUG_ON(!page_count(page));
 
+#ifdef CONFIG_KMEMCHECK
 	/*
 	 * Split shadow pages too, because free(page[0]) would
 	 * otherwise free the whole shadow.
 	 */
 	if (kmemcheck_page_is_tracked(page))
 		split_page(virt_to_page(page[0].shadow), order);
+#endif
 
 	for (i = 1; i < (1 << order); i++)
 		set_page_refcounted(page + i);
