b8aa975b352d53703efc33fb59f5b73e267c7e3f
John Kacur <jkacur@redhat.com>
2009-06-10 16:56:28 +0200

    add latency tracer support for EP93xx boards
    Add latency tracer support for the EP93xx platform. This is done by:
    - adding the correct Kconfig options
    - add (an empty) save_stack_trace implementation.
      -> Someone needs to implement save_stack_trace for arm :)
         Maybe we can use the implementation from rmk?
    - implementing mach_read_cycles (read out EP93XX_TIMER4_VALUE_LOW)
    - implementing mach_cycles_to_usecs (just the same way as for the PXA platform)
    - implementing mach_usecs_to_cycles (just the same way as for the PXA platform)
    
    Signed-off-by: Jan Altenberg <jan@linutronix.de>

---
+++ arch/arm/lib/stacktrace.c
@@ -0,0 +1,7 @@
+#include <linux/sched.h>
+#include <linux/stacktrace.h>
+
+void save_stack_trace(struct stack_trace *trace)
+{
+}
+
--- include/asm-arm/arch-ep93xx/timex.h
+++ include/asm-arm/arch-ep93xx/timex.h
@@ -1,5 +1,11 @@
 /*
  * linux/include/asm-arm/arch-ep93xx/timex.h
  */
+#include <asm-arm/arch-ep93xx/ep93xx-regs.h>
+#include <asm-arm/io.h>
 
 #define CLOCK_TICK_RATE		983040
+
+#define mach_read_cycles() __raw_readl(EP93XX_TIMER4_VALUE_LOW)
+#define mach_cycles_to_usecs(d) (((d) * ((1000000LL << 32) / CLOCK_TICK_RATE)) >> 32)
+#define mach_usecs_to_cycles(d) (((d) * (((long long)CLOCK_TICK_RATE << 32) / 1000000)) >> 32)
