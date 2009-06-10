080f55589eddf41ef352d5364a8e9c8bde2b804f
John Kacur <jkacur@redhat.com>
2009-06-10 16:56:54 +0200

    Implement futex macros for ARM
    Signed-off-by: Khem Raj <kraj@mvista.com>
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: George Davis <gdavis@mvista.com>
    
     arch/arm/kernel/process.c |    2
     include/asm-arm/futex.h   |  125 ++++++++++++++++++++++++++++++++++++++++++++--
     2 files changed, 124 insertions(+), 3 deletions(-)

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -37,6 +37,8 @@
 #include <asm/uaccess.h>
 #include <asm/mach/time.h>
 
+DEFINE_SPINLOCK(futex_atomic_lock);
+
 static const char *processor_modes[] = {
   "USER_26", "FIQ_26" , "IRQ_26" , "SVC_26" , "UK4_26" , "UK5_26" , "UK6_26" , "UK7_26" ,
   "UK8_26" , "UK9_26" , "UK10_26", "UK11_26", "UK12_26", "UK13_26", "UK14_26", "UK15_26",
