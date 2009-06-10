61efcbcad3a01214d0ff8efe83f4e37ef3d99b88
Ingo Molnar <mingo@elte.hu>
2009-06-10 17:01:18 +0200

    Subject: relay: fix timer madness
    remove timer calls (!!!) from deep within the tracing infrastructure.
    This was totally bogus code that can cause lockups and worse.
    Poll the buffer every 2 jiffies for now.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/relay.c
+++ kernel/relay.c
@@ -320,6 +320,10 @@ static void wakeup_readers(unsigned long data)
 {
 	struct rchan_buf *buf = (struct rchan_buf *)data;
 	wake_up_interruptible(&buf->read_wait);
+	/*
+	 * Stupid polling for now:
+	 */
+	mod_timer(&buf->timer, jiffies + 1);
 }
 
 /**
@@ -337,6 +341,7 @@ static void __relay_reset(struct rchan_buf *buf, unsigned int init)
 		init_waitqueue_head(&buf->read_wait);
 		kref_init(&buf->kref);
 		setup_timer(&buf->timer, wakeup_readers, (unsigned long)buf);
+		mod_timer(&buf->timer, jiffies + 1);
 	} else
 		del_timer_sync(&buf->timer);
 
@@ -606,15 +611,6 @@ size_t relay_switch_subbuf(struct rchan_buf *buf, size_t length)
 		buf->subbufs_produced++;
 		buf->dentry->d_inode->i_size += buf->chan->subbuf_size -
 			buf->padding[old_subbuf];
-		smp_mb();
-		if (waitqueue_active(&buf->read_wait))
-			/*
-			 * Calling wake_up_interruptible() from here
-			 * will deadlock if we happen to be logging
-			 * from the scheduler (trying to re-grab
-			 * rq->lock), so defer it.
-			 */
-			__mod_timer(&buf->timer, jiffies + 1);
 	}
 
 	old = buf->data;
