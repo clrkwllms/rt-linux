d10f5be46cd57527975e1d1f4edb40942f2af22f
Gregory Haskins <ghaskins@novell.com>
2009-06-10 16:54:51 +0200

    From ec30f584b4d095d00067850f07f3dc65d587939b Mon Sep 17 00:00:00 2001 Subject: [PATCH] sched: wake-balance fixes
    We have logic to detect whether the system has migratable tasks, but we are
    not using it when deciding whether to push tasks away.  So we add support
    for considering this new information.
    
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -270,6 +270,7 @@ struct rt_rq {
 	unsigned long rt_nr_migratory;
 	/* highest queued rt task prio */
 	int highest_prio;
+	int overloaded;
 };
 
 /*
@@ -6744,6 +6745,7 @@ void __init sched_init(void)
 		rq->migration_thread = NULL;
 		INIT_LIST_HEAD(&rq->migration_queue);
 		rq->rt.highest_prio = MAX_RT_PRIO;
+		rq->rt.overloaded = 0;
 #endif
 		atomic_set(&rq->nr_iowait, 0);
 
