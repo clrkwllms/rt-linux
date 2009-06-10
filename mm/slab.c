35be06a642768e762f0a572050e7accea2c402c6
Andi Kleen <ak@suse.de>
2009-06-10 17:01:57 +0200

    From ak@suse.de Tue Oct 23 16:24:16 2007 To: linux-rt-users@vger.kernel.org Subject: [PATCH] Fix rt preempt slab NUMA freeing
    When this_cpu changes in the free path node needs to change too.
    Otherwise the slab can end up in the wrong node's list and this
    eventually leads to WARN_ONs and of course worse NUMA performace.
    
    This patch is likely not complete (the NUMA slab code is *very* hairy),
    but seems to make the make -j128 test survive for at least two hours.
    
    But at least it fixes one case that regularly triggered during
    testing, resulting in slabs in the wrong node lists and triggering
    WARN_ONs in slab_put/get_obj
    
    I tried a complete audit of keeping this_cpu/node/slabp in sync when needed, but
    it is very hairy code and I likely missed some cases. This so far
    fixes only the simple free path; but it seems to be good enough
    to not trigger easily anymore on a NUMA system with memory pressure.
    
    Longer term the only good fix is probably to migrate to slub.
    Or disable NUMA slab for PREEMPT_RT (its value has been disputed
    in some benchmarks anyways)
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- mm/slab.c
+++ mm/slab.c
@@ -1192,7 +1192,7 @@ cache_free_alien(struct kmem_cache *cachep, void *objp, int *this_cpu)
 	struct array_cache *alien = NULL;
 	int node;
 
-	node = numa_node_id();
+	node = cpu_to_node(*this_cpu);
 
 	/*
 	 * Make sure we are not freeing a object from another node to the array
@@ -4214,6 +4214,8 @@ static void cache_reap(struct work_struct *w)
 		l3 = searchp->nodelists[node];
 
 		work_done += reap_alien(searchp, l3, &this_cpu);
+
+		node = cpu_to_node(this_cpu);
 
 		work_done += drain_array(searchp, l3,
 			    cpu_cache_get(searchp, this_cpu), 0, node);
