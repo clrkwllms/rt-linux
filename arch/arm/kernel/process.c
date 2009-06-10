a245d22e2baf1b2fab3665c478da56a159c6a750
John Kacur <jkacur@redhat.com>
2009-06-10 16:58:26 +0200

    From linux-rt-users-owner@vger.kernel.org Fri Jul 13 20:13:14 2007 Return-Path: <linux-rt-users-owner@vger.kernel.org> X-Spam-Checker-Version: SpamAssassin 3.1.7-deb (2006-10-05) on debian X-Spam-Level: X-Spam-Status: No, score=0.0 required=5.0 tests=AWL autolearn=unavailable 	version=3.1.7-deb Received: from vger.kernel.org (vger.kernel.org [209.132.176.167]) by 	mail.tglx.de (Postfix) with ESMTP id 5902865C3EB; Fri, 13 Jul 2007 20:13:14 	+0200 (CEST) Received: (majordomo@vger.kernel.org) by vger.kernel.org via listexpand id 	S933095AbXGMSNN (ORCPT <rfc822;jan.altenberg@linutronix.de> + 1 other); 	Fri, 13 Jul 2007 14:13:13 -0400 Received: (majordomo@vger.kernel.org) by vger.kernel.org id S933031AbXGMSNM 	(ORCPT <rfc822;linux-rt-users-outgoing>); Fri, 13 Jul 2007 14:13:12 -0400 Received: from deeprooted.net ([216.254.16.51]:38941 "EHLO 	paris.hilman.org" rhost-flags-OK-OK-OK-FAIL) by vger.kernel.org with ESMTP 	id S1760089AbXGMSNH (ORCPT <rfc822;linux-rt-users@vger.kernel.org>); Fri, 	13 Jul 2007 14:13:07 -0400 Received: by paris.hilman.org (Postfix, from userid 1000) id E61B1D2857A; 	Fri, 13 Jul 2007 10:52:28 -0700 (PDT) Message-Id: <20070713175228.623525155@mvista.com> References: <20070713175214.336577416@mvista.com> User-Agent: quilt/0.45-1 Date:	Fri, 13 Jul 2007 10:52:18 -0700 From:	Kevin Hilman <khilman@mvista.com> To:	tglx@linutronix.de, mingo@elte.hu Cc:	linux-rt-users@vger.kernel.org, linux-kernel@vger.kernel.org Subject: [PATCH -rt 4/6] Add trace_preempt_*_idle() support for ARM. Content-Disposition: inline; filename=arm-trace-preempt-idle.patch Sender:	linux-rt-users-owner@vger.kernel.org Precedence: bulk X-Mailing-List:	linux-rt-users@vger.kernel.org X-Filter-To: .Kernel.rt-users X-Evolution-Source: imap://tglx%40linutronix.de@localhost:8993/ Content-Transfer-Encoding: 8bit Mime-Version: 1.0
    Add trace functions to ARM idle loop and also move the
    tick_nohz_restart_sched_tick() after the local_irq_disable() as is
    done on x86.
    
    Signed-off-by: Kevin Hilman <khilman@mvista.com>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -171,11 +171,13 @@ void cpu_idle(void)
 		while (!need_resched() && !need_resched_delayed())
 			idle();
 		leds_event(led_idle_end);
-		tick_nohz_restart_sched_tick();
 		local_irq_disable();
+		trace_preempt_exit_idle();
+		tick_nohz_restart_sched_tick();
 		__preempt_enable_no_resched();
 		__schedule();
 		preempt_disable();
+		trace_preempt_enter_idle();
 		local_irq_enable();
 	}
 }
