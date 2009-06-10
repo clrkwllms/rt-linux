86fcf763a6a3aeae276cc351bb72be4c72bb6ea3
John Kacur <jkacur@redhat.com>
2009-06-10 17:00:06 +0200

    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -2757,6 +2757,9 @@ lock_set_subclass(struct lockdep_map *lock,
 {
 	unsigned long flags;
 
+	if (unlikely(!lock_stat && !prove_locking))
+		return;
+
 	if (unlikely(current->lockdep_recursion))
 		return;
 
