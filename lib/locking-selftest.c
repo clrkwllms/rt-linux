115acfb8f1d12384853beff2e49eed4eaa5d4b36
Ingo Molnar <mingo@elte.hu>
2009-06-13 00:09:07 +0200

    Subject: lockdep: atomic fixup
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- lib/locking-selftest.c
+++ lib/locking-selftest.c
@@ -948,7 +948,7 @@ static void dotest(void (*testcase_fn)(void), int expected, int lockclass_mask)
 	unsigned long saved_preempt_count = preempt_count();
 	int expected_failure = 0;
 #if defined(CONFIG_DEBUG_PREEMPT) && defined(CONFIG_DEBUG_RT_MUTEXES)
-        int saved_lock_count = current->lock_count;
+        atomic_t saved_lock_count = current->lock_count;
 #endif
 
 	WARN_ON(irqs_disabled());
