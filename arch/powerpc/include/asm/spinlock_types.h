ac0028855713faa6010ba01ae6554725ea9bc8ad
Thomas Gleixner <tglx@linutronix.de>
2009-06-17 12:22:20 +0200

    Subject: powerpc-rtmutex.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/powerpc/include/asm/spinlock_types.h
+++ arch/powerpc/include/asm/spinlock_types.h
@@ -7,13 +7,13 @@
 
 typedef struct {
 	volatile unsigned int slock;
-} raw_spinlock_t;
+} __raw_spinlock_t;
 
 #define __RAW_SPIN_LOCK_UNLOCKED	{ 0 }
 
 typedef struct {
 	volatile signed int lock;
-} raw_rwlock_t;
+} __raw_rwlock_t;
 
 #define __RAW_RW_LOCK_UNLOCKED		{ 0 }
 
