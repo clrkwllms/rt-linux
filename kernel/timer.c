c990783e436ae8703377944d7854fdd3a0fbc868
John Kacur <jkacur@redhat.com>
2009-06-24 20:43:05 +0200

    Applied printk-tick-move-to-softirq-on-rt.patch

--- kernel/timer.c
+++ kernel/timer.c
@@ -1182,7 +1182,6 @@ void update_process_times(int user_tick)
 	run_local_timers();
 	if (rcu_pending(cpu))
 		rcu_check_callbacks(cpu, user_tick);
-	printk_tick();
 	run_posix_cpu_timers(p);
 }
 
@@ -1193,6 +1192,7 @@ static void run_timer_softirq(struct softirq_action *h)
 {
 	struct tvec_base *base = per_cpu(tvec_bases, raw_smp_processor_id());
 
+	printk_tick();
 	hrtimer_run_pending();
 
 	if (time_after_eq(jiffies, base->timer_jiffies))
