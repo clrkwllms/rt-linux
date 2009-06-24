603168e48e435cb98da384e250accc9ef99f5f64
John Kacur <jkacur@redhat.com>
2009-06-24 16:53:05 +0200

    Applied pagefault-disable-cleanup.patch

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1376,6 +1376,7 @@ struct task_struct {
 	/* mutex deadlock detection */
 	struct mutex_waiter *blocked_on;
 #endif
+	int pagefault_disabled;
 #ifdef CONFIG_TRACE_IRQFLAGS
 	unsigned int irq_events;
 	int hardirqs_enabled;
