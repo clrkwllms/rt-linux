e5ef7cc1f150ad40a813b3f6bb2e21b22d24b747
Ingo Molnar <mingo@elte.hu>
2009-06-26 17:20:32 +0200

    Subject: preempt: irqs direct debug keyboard
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -307,6 +307,12 @@ extern void scheduler_tick(void);
 
 extern void sched_show_task(struct task_struct *p);
 
+#ifdef CONFIG_GENERIC_HARDIRQS
+extern int debug_direct_keyboard;
+#else
+# define debug_direct_keyboard 0
+#endif
+
 #ifdef CONFIG_DETECT_SOFTLOCKUP
 extern void softlockup_tick(void);
 extern void touch_softlockup_watchdog(void);
