ca959c840b62a6053c4b41cec394593ab659402b
Thomas Gleixner <tglx@linutronix.de>
2009-07-24 15:23:05 +0200

    highmem: Explicitely disable preemption
    With the separation of pagefault_{disable,enable}() from the
    preempt_count a previously overlooked dependancy became painfully
    clear.
    
    kmap_atomic() is per cpu and relies not only on disabling the
    pagefault handler, but really needs preemption disabled too.
    
    Make this explicit now - so that we can change pagefault_disable().
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/mips/mm/highmem.c
+++ arch/mips/mm/highmem.c
@@ -46,7 +46,7 @@ void *__kmap_atomic(struct page *page)
 	unsigned long vaddr;
 	int idx, type;
 
-	/* even !CONFIG_PREEMPT needs this, for in_atomic in do_page_fault */
+	preempt_disable();
 	pagefault_disable();
 	if (!PageHighMem(page))
 		return page_address(page);
@@ -71,6 +71,7 @@ void __kunmap_atomic(void *kvaddr)
 
 	if (vaddr < FIXADDR_START) { // FIXME
 		pagefault_enable();
+		preempt_enable();
 		return;
 	}
 
@@ -91,6 +92,7 @@ void __kunmap_atomic(void *kvaddr)
 #endif
 	kmap_atomic_idx_pop();
 	pagefault_enable();
+	preempt_enable();
 }
 EXPORT_SYMBOL(__kunmap_atomic);
 
@@ -103,6 +105,7 @@ void *kmap_atomic_pfn(unsigned long pfn)
 	unsigned long vaddr;
 	int idx, type;
 
+	preempt_disable();
 	pagefault_disable();
 
 	type = kmap_atomic_idx_push();
