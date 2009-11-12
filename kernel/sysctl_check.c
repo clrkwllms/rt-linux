8f02c708e4f9363fa1ca6ca1b42c7144755073c9
Clark Williams <williams@redhat.com>
2009-11-12 13:28:02 +0100

    OOM kill modifications for RHEL
    Signed-off-by: Larry Woodman <lwoodman@redhat.com>
    Signed-off-by: Clark Williams <williams@redhat.com>

--- kernel/sysctl_check.c
+++ kernel/sysctl_check.c
@@ -135,6 +135,7 @@ static const struct trans_ctl_table trans_vm_table[] = {
 	{ VM_PANIC_ON_OOM,		"panic_on_oom" },
 	{ VM_VDSO_ENABLED,		"vdso_enabled" },
 	{ VM_MIN_SLAB,			"min_slab_ratio" },
+	{ VM_OOM_KILL,			"oom_kill_enabled" },
 
 	{}
 };
