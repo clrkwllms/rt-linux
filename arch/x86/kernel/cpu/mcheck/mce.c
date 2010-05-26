bb9cb069897b3ad476fcc726cf267fe3d28fd6fa
Vernon Mauery <vernux@us.ibm.com>
2010-05-26 12:19:49 +0200

    i7core_edac: Bring the i7core_edac up to date with linux-head
    Bring the i7core_edac up to date with linux-head
    
    Signed-off-by: Vernon Mauery <vernux@us.ibm.com>
    Signed-off-by: John Kacur <jkaucr@redhat.com>

--- arch/x86/kernel/cpu/mcheck/mce.c
+++ arch/x86/kernel/cpu/mcheck/mce.c
@@ -35,6 +35,7 @@
 #include <linux/fs.h>
 #include <linux/mm.h>
 #include <linux/debugfs.h>
+#include <linux/edac_mce.h>
 
 #include <asm/processor.h>
 #include <asm/hw_irq.h>
@@ -160,6 +161,14 @@ void mce_log(struct mce *mce)
 	for (;;) {
 		entry = rcu_dereference(mcelog.next);
 		for (;;) {
+			/* If edac_mce is enabled, it will check the error type
+			 * and will process it, if it is a known error.
+			 * Otherwise, the error will be sent through mcelog
+			 * interface
+			 */
+			if (edac_mce_parse(mce))
+				return;
+
 			/*
 			 * When the buffer fills up discard new entries.
 			 * Assume that the earlier errors are the more
@@ -193,6 +202,8 @@ void mce_log(struct mce *mce)
 
 static void print_mce(struct mce *m)
 {
+	edac_mce_parse(m);
+
 	pr_emerg("CPU %d: Machine Check Exception: %16Lx Bank %d: %016Lx\n",
 	       m->extcpu, m->mcgstatus, m->bank, m->status);
 
