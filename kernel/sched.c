a77eaf3d8062ace3fe1d60b819a2b8a5c6c5de1a
Thomas Gleixner <tglx@linutronix.de>
2011-03-21 13:32:17 +0100

    preempt-mark-legitimated-no-resched-sites.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched.c
+++ kernel/sched.c
@@ -4243,7 +4243,7 @@ need_resched:
 
 	post_schedule(rq);
 
-	preempt_enable_no_resched();
+	__preempt_enable_no_resched();
 	if (need_resched())
 		goto need_resched;
 }
@@ -4291,7 +4291,7 @@ EXPORT_SYMBOL(schedule);
  */
 void __sched schedule_preempt_disabled(void)
 {
-	preempt_enable_no_resched();
+	__preempt_enable_no_resched();
 	schedule();
 	preempt_disable();
 }
@@ -5512,7 +5512,7 @@ SYSCALL_DEFINE0(sched_yield)
 	__release(rq->lock);
 	spin_release(&rq->lock.dep_map, 1, _THIS_IP_);
 	do_raw_spin_unlock(&rq->lock);
-	preempt_enable_no_resched();
+	__preempt_enable_no_resched();
 
 	schedule();
 
