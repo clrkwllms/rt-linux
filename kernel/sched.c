a9070eb65ad9dd40d45527496ddefb7cb01f3734
Thomas Gleixner <tglx@linutronix.de>
2011-07-14 09:56:44 +0200

    cond-resched-softirq-fix.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched.c
+++ kernel/sched.c
@@ -5665,6 +5665,7 @@ int __cond_resched_lock(spinlock_t *lock)
 }
 EXPORT_SYMBOL(__cond_resched_lock);
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 int __sched __cond_resched_softirq(void)
 {
 	BUG_ON(!in_softirq());
@@ -5678,6 +5679,7 @@ int __sched __cond_resched_softirq(void)
 	return 0;
 }
 EXPORT_SYMBOL(__cond_resched_softirq);
+#endif
 
 /**
  * yield - yield the current processor to other threads.
