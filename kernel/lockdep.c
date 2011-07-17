343f36d0e1c419e72d61a5311b66fffe57645b55
Thomas Gleixner <tglx@linutronix.de>
2011-07-17 18:51:23 +0200

    lockdep-rt.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -3488,6 +3488,7 @@ static void check_flags(unsigned long flags)
 		}
 	}
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 	/*
 	 * We dont accurately track softirq state in e.g.
 	 * hardirq contexts (such as on 4KSTACKS), so only
@@ -3502,6 +3503,7 @@ static void check_flags(unsigned long flags)
 			DEBUG_LOCKS_WARN_ON(!current->softirqs_enabled);
 		}
 	}
+#endif
 
 	if (!debug_locks)
 		print_irqtrace_events(current);
