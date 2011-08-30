f4c596657628189ae108ebb6dc42dc377f1f3487
Stratos Psomadakis <psomas@gentoo.org>
2011-08-30 12:48:22 +0300

    sched: Fix sched_debug.c CONFIG_PREEMPT_RT_FULL=n build
    When CONFIG_PREEMPT_RT_FULL is not set, a build error occurs due to
    migrate_disable task_struct member not existing. 
    
    Trivial fix, enclose it inside an #ifdef
    
    Signed-off-by: Stratos Psomadakis <psomas@gentoo.org>
    Cc: peterz@infradead.org
    Link: http://lkml.kernel.org/r/1314697702-19946-1-git-send-email-psomas@gentoo.org
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched_debug.c
+++ kernel/sched_debug.c
@@ -485,7 +485,9 @@ void proc_sched_show_task(struct task_struct *p, struct seq_file *m)
 	P(se.load.weight);
 	P(policy);
 	P(prio);
+#ifdef CONFIG_PREEMPT_RT_FULL
 	P(migrate_disable);
+#endif
 	P(rt.nr_cpus_allowed);
 #undef PN
 #undef __PN
